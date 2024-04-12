274635367d231c9ce7d034b78b3a579b69402ed5
Reza Hussain <alirezaa08@gmail.com>
2024-04-13 02:17:24 +0530

    Fixed build issues

--- services/apiUrl.ts
+++ services/apiUrl.ts
@@ -1,4 +1,5 @@
-const BASE_URL = "http://localhost:8080";
+// const BASE_URL = "http://localhost:8080";
+const BASE_URL = "https://occulead-backend.onrender.com";
 
 // auth
 export const REGISTER_USER = `${BASE_URL}/auth/registerUser`;
